69e4db94f08fcdb81ef40bae05d89c6142695c90
rob.lasota <rob.lasota@fahlgren.com>
2016-04-28 15:44:38 -0400

    initilal

---
+++ src/Services/GoogleRecaptcha.php
@@ -0,0 +1,42 @@
+<?php
+namespace LaSota\LaravelBase\Services;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+/**
+ * Class GoogleRecaptcha
+ *
+ * @package LaSota\LaravelBase\Services
+ */
+class GoogleRecaptcha
+{
+    public static function verify($input)
+    {
+        if ( env('RECAPTCHA_SKIP') ) {
+            return true;
+        }
+
+        if (empty($input['g-recaptcha-response'])) {
+            return false;
+        }
+
+        $client = new GuzzleClient();
+
+        $endpoint = 'https://www.google.com/recaptcha/api/siteverify';
+
+        $params = [
+            'response' => $input['g-recaptcha-response'],
+            'remoteip' => $input['remoteip'],
+            'secret' => env('RECAPTCHA_SECRET'),
+        ];
+
+        $url = implode('?', [$endpoint, http_build_query($params)]);
+
+        $response = $client->get($url);
+        $response = json_decode( $response->getBody()->getContents(), true);
+
+        $success = array_get($response, 'success') ? true: false;
+
+        return $success;
+    }
+}
